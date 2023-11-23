55d498c967e3ac45c40753a13ec6a11283aa7f0c
MuhRandy <muhammadrandy0027@gmail.com>
2023-11-24 06:52:12 +0700

    Getting & Saving Data

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express app
 const app = express();
 
 // connect to mongodb
 const dbURI =
-  "mongodb+srv://newdev0027:181101@cluster0.wqnnoge.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://newdev0027:181101@cluster0.wqnnoge.mongodb.net/node-tuts?retryWrites=true&w=majority";
 mongoose
   .connect(dbURI)
   .then((result) => app.listen(3000))
@@ -19,6 +20,34 @@ app.set("view engine", "ejs");
 // middleware & static files
 app.use(express.static("public"));
 app.use(morgan("dev"));
+
+// mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err));
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("655fe15b8bddba2786240246")
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err));
+});
 
 app.get("/", (req, res) => {
   const blogs = [
@@ -41,10 +70,3 @@ app.get("/blogs/create", (req, res) => {
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
-
-// Mogoose is an ODM library - Object Document Mapping library
-
-// Schemas defines the structure of atype of data/document
-// -Properties & property types
-
-// Models allow us to communicate with database collections
