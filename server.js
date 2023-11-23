1bf6d9945dbd4f5e1cf3b3ac259284a60137212f
MuhRandy <muhammadrandy0027@gmail.com>
2023-11-23 08:40:53 +0700

    response

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  res.write("<head><link rel='stylesheet' href='#'></head>");
+  res.write("<p>Hello, Randy<p>");
+  res.write("<p>Hello again, Randy<p>");
+  res.end();
 });
 
 // localhost is like a domain name on the web, but the domain is from our own computer
