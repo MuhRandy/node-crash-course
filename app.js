b145b7f4e1edf30f39365cf9d51bae71f526c892
MuhRandy <muhammadrandy0027@gmail.com>
2023-11-23 20:32:24 +0700

    Middleware

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.set("view engine", "ejs");
 // listen for request
 app.listen(3000);
 
+app.use((req, res) => {
+  console.log("new request made");
+  console.log("host: ", req.hostname);
+  console.log("path: ", req.path);
+  console.log("method: ", req.method);
+});
+
 app.get("/", (req, res) => {
   const blogs = [
     { title: "Yada yada", snippet: "Lorem ipsum dolor sit amet consectetur" },
@@ -31,4 +38,9 @@ app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
 
-// EJS Templates are processed through the EJS view engine on the server
+// Middleware is code which runs (on the server) between getting a request and sending a response
+// Examples
+// -Logger middleware to log details of every request
+// -Authentication check middleware for protected routes
+// -Middleware to parse JSON data from requests
+// -Return 404 pages
