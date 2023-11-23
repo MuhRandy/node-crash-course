5c3eeb453165fa27d25c9f61cea05e04cf6d0538
unknown <muhammadrandy0027@gmail.com>
2023-11-23 10:02:41 +0800

    basic routes

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
-const http = require("http");
+const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log("request made");
+  console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader('Content-Type', 'text/html');
+
+  let path = './views/';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      break;
+    case '/about':
+      path += 'about.html';
+      break;
+    default:
+      path += '404.html';
+  }
+
+  // send an html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      // res.write(data);
+      res.end(data);
+    }
+  });
 });
 
 // localhost is like a domain name on the web, but the domain is from our own computer
 // Port number are like 'doors' into a computer, as your port number doesnt clash on other port numbers it can be used
-server.listen(3000, "localhost", () => {
-  console.log("listening for request on port 3000");
+server.listen(3000, 'localhost', () => {
+  console.log('listening for request on port 3000');
 });
