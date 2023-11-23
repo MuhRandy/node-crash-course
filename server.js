59679ae4696ee6243064e04b0afcc95beef99cb6
MuhRandy <muhammadrandy0027@gmail.com>
2023-11-23 08:26:49 +0700

    server

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("request made");
+});
+
+// localhost is like a domain name on the web, but the domain is from our own computer
+// Port number are like 'doors' into a computer, as your port number doesnt clash on other port numbers it can be used
+server.listen(3000, "localhost", () => {
+  console.log("listening for request on port 3000");
+});
