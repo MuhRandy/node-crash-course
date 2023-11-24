a8e716b5801852bbe4a2600c76ea04769ceef75a
MuhRandy <muhammadrandy0027@gmail.com>
2023-11-24 07:03:16 +0700

    Outputting Documents in Views

--- app.js
+++ app.js
@@ -21,45 +21,23 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(morgan("dev"));
 
-// mongoose and mongo sandbox routes
-app.get("/add-blog", (req, res) => {
-  const blog = new Blog({
-    title: "new blog",
-    snippet: "about my new blog",
-    body: "more about my new blog",
-  });
-
-  blog
-    .save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => console.log(err));
-});
-
-app.get("/all-blogs", (req, res) => {
-  Blog.find()
-    .then((result) => res.send(result))
-    .catch((err) => console.log(err));
-});
-
-app.get("/single-blog", (req, res) => {
-  Blog.findById("655fe15b8bddba2786240246")
-    .then((result) => res.send(result))
-    .catch((err) => console.log(err));
-});
-
+// routes
 app.get("/", (req, res) => {
-  const blogs = [
-    { title: "Yada yada", snippet: "Lorem ipsum dolor sit amet consectetur" },
-    { title: "Bla h blah", snippet: "Lorem ipsum dolor sit amet consectetur" },
-    { title: "Mora Lisa", snippet: "Lorem ipsum dolor sit amet consectetur" },
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+// blog routes
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => console.log(err));
 });
 
 app.get("/blogs/create", (req, res) => {
